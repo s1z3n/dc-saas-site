d3a1c26e718ec9292e778dd9ef610051be3f27a2
Ali Beheshti <alibeheshti@triaset.com>
2017-02-21 13:44:28 +0330

    Importing files

---
+++ assets/js/functions.js
@@ -0,0 +1,208 @@
+$(document).ready(function() {
+
+
+  $('.contact-nav').click(function(){
+  	$('.modal-bg').addClass('is-open');
+  
+  });
+
+   $('.close').click(function(){
+  	$('.modal-bg').removeClass('is-open');
+  });
+
+   $('.modal-bg').click(function(event){
+   	if (event.target == this){
+   		$('.modal-bg').removeClass('is-open');
+   	}
+
+  	
+  });
+
+  $('#Account-Assistant').click(function(){
+    $('.job-modal-bg-1').addClass('is-open');
+ 
+  });
+
+   $('.job-close').click(function(){
+    $('.job-modal-bg-1').removeClass('is-open');
+  });
+
+   $('.job-modal-bg-1').click(function(event){
+    if (event.target == this){
+      $('.job-modal-bg-1').removeClass('is-open');
+    }
+
+    
+  });
+
+
+
+$('#Application-Consultant').click(function(){
+    $('.job-modal-bg-2').addClass('is-open');
+  
+  });
+
+   $('.job-close').click(function(){
+    $('.job-modal-bg-2').removeClass('is-open');
+  });
+
+   $('.job-modal-bg-2').click(function(event){
+    if (event.target == this){
+      $('.job-modal-bg-2').removeClass('is-open');
+    }
+
+    
+  });
+
+
+
+
+
+$('#Business-Consultant').click(function(){
+    $('.job-modal-bg-3').addClass('is-open');
+    // return false
+  });
+
+   $('.job-close').click(function(){
+    $('.job-modal-bg-3').removeClass('is-open');
+  });
+
+   $('.job-modal-bg-3').click(function(event){
+    if (event.target == this){
+      $('.job-modal-bg-3').removeClass('is-open');
+    }
+
+    
+  });
+
+
+
+
+
+
+
+
+$('#Business-Development-Manager').click(function(){
+    $('.job-modal-bg-4').addClass('is-open');
+    // return false
+  });
+
+   $('.job-close').click(function(){
+    $('.job-modal-bg-4').removeClass('is-open');
+  });
+
+   $('.job-modal-bg-4').click(function(event){
+    if (event.target == this){
+      $('.job-modal-bg-4').removeClass('is-open');
+    }
+
+    
+  });
+
+
+
+
+
+
+
+
+
+
+
+$('#Marketing-Executive').click(function(){
+    $('.job-modal-bg-5').addClass('is-open');
+    // return false
+  });
+
+   $('.job-close').click(function(){
+    $('.job-modal-bg-5').removeClass('is-open');
+  });
+
+   $('.job-modal-bg-5').click(function(event){
+    if (event.target == this){
+      $('.job-modal-bg-5').removeClass('is-open');
+    }
+
+    
+  });
+
+
+
+
+$('#Software-Engineer').click(function(){
+    $('.job-modal-bg-6').addClass('is-open');
+    // return false
+  });
+
+   $('.job-close').click(function(){
+    $('.job-modal-bg-6').removeClass('is-open');
+  });
+
+   $('.job-modal-bg-6').click(function(event){
+    if (event.target == this){
+      $('.job-modal-bg-6').removeClass('is-open');
+    }
+
+    
+  });
+
+
+   
+   $('#System-Admin').click(function(){
+    $('.job-modal-bg-7').addClass('is-open');
+    // return false
+  });
+
+   $('.job-close').click(function(){
+    $('.job-modal-bg-7').removeClass('is-open');
+  });
+
+   $('.job-modal-bg-7').click(function(event){
+    if (event.target == this){
+      $('.job-modal-bg-7').removeClass('is-open');
+    }
+
+    
+  });
+
+
+
+
+
+
+$('#Technical-Support-Consultant').click(function(){
+    $('.job-modal-bg-8').addClass('is-open');
+    // return false
+  });
+
+   $('.job-close').click(function(){
+    $('.job-modal-bg-8').removeClass('is-open');
+  });
+
+   $('.job-modal-bg-8').click(function(event){
+    if (event.target == this){
+      $('.job-modal-bg-8').removeClass('is-open');
+    }
+
+    
+  });
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
