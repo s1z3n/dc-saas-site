cb50e3faa1988f0eea56a757c16c8ea1cd94db54
Ali Beheshti <alibeheshti@triaset.com>
2017-05-06 22:56:20 +0430

    test

---
+++ _site/assets/js/functions.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+  $('.menu-btn').click(function(){
+  	$('.navigation').addClass('is-open');
+
+  });
+
+  //  $('.close').click(function(){
+  // 	$('.modal-bg').removeClass('is-open');
+  // });
+  //
+  //  $('.navigation').click(function(event){
+  //  	if (event.target == this){
+  //  		$('.navigation').removeClass('is-open');
+  //  	}
